d464652b7a004084daa398a3186be7d3541502f7
Matthew Camarena <camarena.matthew@gmail.com>
2024-03-13 13:10:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,40 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// My Solution
+var urlLinks = [];
+app.post('/api/shorturl', (req,res)=>{
+  var bodyUrl = req.body.url; 
+  var splitUrl = bodyUrl.split("//",2);
+
+  if(splitUrl[0] == "http:" || splitUrl[0] == "https:"){
+    dns.lookup(splitUrl[1], (err)=>{
+      if(err){
+        res.json({error: 'invalid url'});
+      }else{
+        //if its not in the list add it
+        if(!urlLinks.includes(bodyUrl)){
+          urlLinks.push(bodyUrl);
+        }
+        res.json({original_url: bodyUrl, short_url: urlLinks.indexOf(bodyUrl)});
+        
+      }
+    })
+  }else{
+    res.json({error: 'invalid url'});
+  }
+
+})
+app.get('/api/shorturl/:shortUrl?',(req,res)=>{
+  var shortUrl = req.params.shortUrl;
+  if(Number.isInteger(parseInt(shortUrl))){
+    res.redirect(urlLinks[shortUrl])
+  }else{
+    res.json({error: "wrong format"});
+  }
+  
+})
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
